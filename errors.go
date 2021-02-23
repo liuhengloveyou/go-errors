9adaae4bf975dee8d166476df5c0ead6070acfe9
liuheng <liuhengloveyou@gmail.com>
2021-02-23 11:13:20 +0800

    add NewError

--- errors.go
+++ errors.go
@@ -17,9 +17,20 @@ var (
 type Error struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
-	ID      string `json:"id"`
+	ID      string `json:"id,omitempty"`
 }
 
 func (e Error) Error() string {
 	return fmt.Sprintf("%d %s", e.Code, e.Message)
+}
+
+func NewError(code int, message string) Error {
+	key := fmt.Sprintf("%d", code)
+	if _, exist := errCodeDefined[key]; exist {
+		panic(fmt.Sprintf("error code %s already exist", key))
+	}
+
+	errCodeDefined[key] = true
+
+	return Error{Code: code, Message: message}
 }
